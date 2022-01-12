34ffeffa42d973c133e363421affdab153afa7ed
Anderson Melo <contato.andersonmelo@hotmail.com>
2022-01-12 11:33:29 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,35 @@
 import Pagination from "components/Pagination";
-import MovieStars from "components/MovieStars";
+import MovieCard from "components/MovieCard";
 
 
 function Listing() {
 
-    return(
+    return (
         <>
-        <Pagination/>
-        <MovieStars/>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+
+
+            </div>
+
         </>
     );
 }
